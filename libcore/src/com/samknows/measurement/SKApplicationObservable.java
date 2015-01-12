cdc6299b578294f1743c623c33a04c8fad0e263c
Eugene Wise <eugene@samknows.com>
2015-01-12 22:58:15 +0000

    Work in progress

---
+++ libcore/src/com/samknows/measurement/SKApplicationObservable.java
@@ -0,0 +1,7 @@
+package com.samknows.measurement;
+
+import java.util.Observable;
+
+public class SKApplicationObservable extends Observable {
+
+}
--- libcore/test/com/samknows/libcore/JLatency.java
+++ libcore/test/com/samknows/libcore/JLatency.java
@@ -2,14 +2,46 @@ package com.samknows.libcore;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+import java.util.ArrayList;
+
+
+
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
 import com.samknows.tests.Latency;
+import com.samknows.tests.Param;
+
 
 @RunWith(RobolectricTestRunner.class)
 public class JLatency {
+	
+	/*	<param name="target" value="$closest"/>
+	<param name="port" value="6000"/>
+	<param name="interPacketTime" value="500000"/>
+	<param name="delayTimeout" value="2000000"/>
+	<param name="numberOfPackets" value="60"/>
+	<param name="percentile" value="100"/>
+	<param name="maxTime" value="30000000"/>
+*/	
+
+	List<Param> params = new ArrayList<Param>();
+
+	public JLatency(){
+		params.add(new Param("target", "n1-the1.samknows.com"));
+		params.add(new Param("port", "6000"));
+		params.add(new Param("intrPacketTime", "500000"));
+		params.add(new Param("delayTimeout", "2000000"));
+		params.add(new Param("numberOfPackets", "60"));
+		params.add(new Param("percentile", "100"));
+		params.add(new Param("maxTime", "30000000"));
+		
+		latency = new Latency(params);
+	}
+	
 	
 	Latency latency = null;
 
@@ -35,7 +67,7 @@ public class JLatency {
 
 	@Test
 	public final void testIsReady() {
-		fail("Not yet implemented"); // TODO
+		assertTrue(latency.isReady());
 	}
 
 	@Test
